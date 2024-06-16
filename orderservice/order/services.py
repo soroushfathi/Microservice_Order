b9e8e536256e3c75ccf37d75e3b060bf2f661730
soroushfathi <soroush8fathi@gmail.com>
2024-06-16 22:34:57 +0330

    fix name error

--- orderservice/order/services.py
+++ orderservice/order/services.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 import requests
 
 
-def create_order(product_id: int, price: float, quantity: int):
+def create_order(product_id: int, product_price: float, quantity: int):
     total_price = quantity * product_price
 
     order = Order.objects.create(
