11cd91b312a718e9ac08631053bb192c58dac0f0
soroushfathi <soroush8fathi@gmail.com>
2024-06-16 21:19:57 +0330

    new API: Create Order: 1. fetch detail 2. validate product and stock 3. cacl price

--- orderservice/order/views.py
+++ orderservice/order/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from orderservice.api.utils import format_response
+from orderservice.utils.product_service import ProductService
+from .serializers import OrderCreateSerializer
+
+
+product_service = ProductService()
+
+
+class CreateOrder(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        serializer = OrderCreateSerializer(data=request.data)
+        if not serializer.is_valid():
+            fresponse = format_response(
+                success=False,
+                message=serializer.errors,
+            )
+            return Response(fresponse, status=status.HTTP_400_BAD_REQUEST)
+
+        product_id = serializers.validated_data['product_id']
+        quantity = serializers.validated_data['quantity']
+        product = product_service.get_product(product_id=product_id)
+        if not product['status']:
+            fresponse = format_response(success=False,
+                    message="Product Not Found")
+            return Response(fresponse, statuss=status.HTTP_404_NOT_FOUND)
+
+        product_data = product['data']
+        if quantity > product_data['stock']:
+            fresponse = format_response(success=False,
+                    message="Not Available Stock for this product")
+            return Response(fresponse, status=status.HTTP_400_BAD_REQUEST)
+
+        total_price = quantity * float(product_data['price'])
+
+        # Save Order
+        ...
