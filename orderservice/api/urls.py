89a3bd299abfa42d5ec7f3724cd4ec98a1af6c7b
soroushfathi <soroush8fathi@gmail.com>
2024-06-16 22:29:46 +0330

    include auth and users urls

--- orderservice/api/urls.py
+++ orderservice/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
+    path('users/', include(('orderservice.users.urls', 'users'))),
+    path('auth/', include(('orderservice.authentication.urls', 'auth'))),
     path('order/', include(('orderservice.order.urls', 'order')))
 ]
