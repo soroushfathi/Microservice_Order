89aa756cd0195990c65ac421fe5d0559ef505aca
soroushfathi <soroush8fathi@gmail.com>
2024-06-16 08:21:40 +0330

    new model: Order

--- orderservice/order/models.py
+++ orderservice/order/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    product_id = models.IntegerField()
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order of {self.quantity} units of product ID {self.product_id}"
+
