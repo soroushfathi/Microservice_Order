8cea588dab5b4c395424644bb003de937b3c77d0
soroushfathi <soroush8fathi@gmail.com>
2024-06-17 12:48:08 +0330

    change permissions

--- docker/web_entrypoint.sh
+++ docker/web_entrypoint.sh
@@ -3,14 +3,15 @@
 echo "--> Waiting for db to be ready"
 ./wait-for-it.sh db:5432
 
+cd OrderService
+
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py makemigration
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --clear --noinput
 python manage.py collectstatic --noinput
 
 # Start server
 echo "--> Starting web process"
-cd OrderService
-gunicorn config.wsgi:application -b 0.0.0.0:8000
+gunicorn config.wsgi:application -b 0.0.0.0:8002
